019b18c997057ea0c39e36ea75911b398b45dbab
stephen bilham <stephenbilham@yahoo.com>
2020-02-14 12:46:19 -0800

    hid dice at start and connected element to random number once rolled

--- starter/app.js
+++ starter/app.js
@@ -8,3 +8,34 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScores, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  //random num
+  var dice = Math.floor(Math.random() * 6 + 1);
+
+  // display results
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+
+  // update the round if roll isnt 1
+});
+
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+// document.querySelector("#current-" + activePlayer).innerHTML =
+//   "<em>" + dice + "<em/>";
+// var x = document.querySelector("#score-0").textContent;
+// console.log(x);
