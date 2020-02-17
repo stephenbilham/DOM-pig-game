618904432a2f0f9e4c9c63f903c8479af752f678
stephen bilham <stephenbilham@yahoo.com>
2020-02-17 14:19:24 -0800

    added another dice and more rules

--- starter/app.js
+++ starter/app.js
@@ -1,30 +1,31 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
 var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
+// var lastDice; if there was one dice this is how you get the last roll! important.
+
 document.querySelector(".btn-roll").addEventListener("click", function() {
   if (gamePlaying === true) {
     //random num
-    var dice = Math.floor(Math.random() * 6 + 1);
+    var dice1 = Math.floor(Math.random() * 6 + 1);
+    var dice2 = Math.floor(Math.random() * 6 + 1);
+    console.log(dice1);
+    console.log(dice2);
+
     // display results
-    var diceDOM = document.querySelector(".dice");
-    diceDOM.style.display = "block";
-    diceDOM.src = "dice-" + dice + ".png";
-    // update the round if roll isnt 1
-    if (dice !== 1) {
+    document.getElementById("dice-1").style.display = "block";
+    document.getElementById("dice-2").style.display = "block";
+
+    document.getElementById("dice-1").src = "dice-" + dice1 + ".png";
+    document.getElementById("dice-2").src = "dice-" + dice2 + ".png";
+
+    if (dice1 === 6 && dice2 === 6) {
+      scores[activePlayer] = 0;
+      document.querySelector("#score-" + activePlayer).textContent = "0";
+      nextPlayer();
+    } else if (dice1 !== 1 && dice2 !== 1) {
       // add score
-      roundScore += dice;
+      roundScore += dice1 + dice2;
       document.querySelector(
         "#current-" + activePlayer
       ).textContent = roundScore;
@@ -32,6 +33,7 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
       // next player
       nextPlayer();
     }
+    // lastDice = dice; * if there was one dice
   }
 });
 
@@ -42,11 +44,21 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
     // update UI
     document.getElementById("score-" + activePlayer).textContent =
       scores[activePlayer];
-    // check if player won game
 
-    if (scores[activePlayer] >= 100) {
+    //input score
+    var input = document.querySelector(".final-score").value;
+
+    if (input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+
+    // check if player won game
+    if (scores[activePlayer] >= winningScore) {
       document.querySelector("#name-" + activePlayer).textContent = "winner!";
-      document.querySelector(".dice").style.display = "none";
+      document.getElementById("dice-1").style.display = "none";
+      document.getElementById("dice-2").style.display = "none";
       document
         .querySelector(".player-" + activePlayer + "-panel")
         .classList.add("winner");
@@ -68,7 +80,8 @@ function init() {
   activePlayer = 0;
   gamePlaying = true;
 
-  document.querySelector(".dice").style.display = "none";
+  document.getElementById("dice-1").style.display = "none";
+  document.getElementById("dice-2").style.display = "none";
 
   document.getElementById("score-0").textContent = "0";
   document.getElementById("score-1").textContent = "0";
@@ -90,12 +103,6 @@ function nextPlayer() {
 
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
-  // document.querySelector(".player-0-panel").classList.remove("active");
-  // document.querySelector(".player-1-panel").classList.add("active");
-  document.querySelector(".dice").style.display = "none";
+  document.getElementById("dice-1").style.display = "block";
+  document.getElementById("dice-2").style.display = "block";
 }
-
-// document.querySelector("#current-" + activePlayer).innerHTML =
-//   "<em>" + dice + "<em/>";
-// var x = document.querySelector("#score-0").textContent;
-// console.log(x);
