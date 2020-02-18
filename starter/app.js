64503ec0fff7387746fd437464aa26e97bc27f0e
stephen bilham <stephenbilham@yahoo.com>
2020-02-17 18:30:41 -0800

    fixed bug on reset

--- starter/app.js
+++ starter/app.js
@@ -94,6 +94,7 @@ function init() {
   document.querySelector(".player-0-panel").classList.remove("winner");
   document.querySelector(".player-1-panel").classList.remove("winner");
   document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
   document.querySelector(".player-0-panel").classList.add("active");
 }
 
